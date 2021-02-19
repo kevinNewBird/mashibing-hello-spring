ba6801adc77e9bb9073c7f9111b3485cb0fe1420
zhao.song <song0586@126.com>
2021-02-19 17:28:51 +0800

    feat(gradle): gradle工程初始化

---
+++ src/main/java/com/mashibing/demo/HelloSpringTest.java
@@ -0,0 +1,14 @@
+package com.mashibing.demo;
+
+/***********************
+ * @Description: TODO 类描述<BR>
+ * @author: zhao.song
+ * @since: 2021/2/19 17:27
+ * @version: 1.0
+ ***********************/
+public class HelloSpringTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
